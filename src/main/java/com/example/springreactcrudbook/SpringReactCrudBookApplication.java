ad168065c17e3b192d1b497e3fe40238f2aca433
Hasan Polat <thatsnotpublic@gmail.com>
2023-03-09 22:38:01 +0300

    init

---
+++ src/main/java/com/example/springreactcrudbook/SpringReactCrudBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.springreactcrudbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactCrudBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactCrudBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springreactcrudbook/SpringReactCrudBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springreactcrudbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactCrudBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
