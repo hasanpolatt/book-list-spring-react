a4cc865104026ca6cfd76a115a897a4d409decca
Hasan Polat <thatsnotpublic@gmail.com>
2023-03-11 19:23:09 +0300

    added Navbar and Home

---
+++ src/frontend/src/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+import {Component} from "react";
+import AppNavbar from "./AppNavbar";
+import {Button} from "reactstrap";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <AppNavbar/>
+                <Container fluid>
+                    <Button color="link"><Link to="/books">Books</Link></Button>
+                </Container>
+            </div>
+        )
+    }
+}
+
+export default Home;
